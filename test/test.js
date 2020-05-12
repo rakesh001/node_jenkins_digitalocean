714d2d468edf0722c0748485c9168e55e4ca7e8b
Raka <rakeshk@kwixglobal.com>
2020-05-12 05:59:55 +0530

    changes

--- test/test.js
+++ test/test.js
@@ -1,9 +1,19 @@
-var supertest = require("supertest");
+var request = require("supertest");
 var should = require("should");
 
 // This agent refers to PORT where the program is running.
 
-var server = supertest.agent("http://localhost:4000");
+//var server = supertest.agent("http://localhost:4000");
+var server = require('../app.js');
+
+// var request = require(‘supertest’);
+// var app = require(‘../index.js’);
+// describe(‘GET /’, function() {
+//  it(‘respond with hello world’, function(done) {
+//  //navigate to root and check the the response is "hello world"
+//  request(app).get(‘/’).expect(‘hello world’, done);
+//  });
+// });
 
 // UNIT test begin
 
@@ -12,7 +22,7 @@ describe("SAMPLE unit test",function(){
   // #1 should return home page
   it("should return home page",function(done){
     // calling home page
-    server
+    request(server)
     .get("/")
     //.expect("Content-type",/text/)
     .expect(200) // THis is HTTP response
